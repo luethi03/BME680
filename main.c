02027dd1faf28cb7388a31e9d433599db95f467e
Samuel <samuel.luethi@8460.ch>
2022-12-14 09:00:46 +0100

    ADD BME STUFF
    add header.h and main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+ * BME680.c
+ *
+ * Created: 12.12.2022 10:01:24
+ * Author : Samuel Lüthi
+ */ 
+
+#include "Header.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
+char INIT ( void )
+{
+	
+}
